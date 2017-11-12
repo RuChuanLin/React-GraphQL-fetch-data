f3cab86e8615bd36a661d6b5f0de6ed4f493d754
cheneddie <eddie067@hotmail.com>
2017-11-12 22:51:56 +0800

    updata

--- server/schema/teacher_type.js
+++ server/schema/teacher_type.js
@@ -4,7 +4,6 @@ const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList } = graphql;
 const TeacherType = new GraphQLObjectType({
   name: 'TeacherType',
   fields: () => ({
-    id: { type: GraphQLID },
     teacherName: { type: GraphQLString },
     experience: { type: GraphQLString },
     description: { type: GraphQLString },
